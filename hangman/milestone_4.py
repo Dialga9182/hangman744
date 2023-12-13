44073914a4d6c747a73da19e66e1c401e93718ec
Dialga9182 <85892628+Dialga9182@users.noreply.github.com>
2023-12-13 16:23:34 +0000

    Updates milestone_4.py
    This adds somewhat comprehensible docstrings to Hangman class, check_guess, and ask_for_input methods in milestone_4.py. Also removes a redundant triple quote string.

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -3,30 +3,58 @@ import random
 word_list = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry']
 
 class Hangman:
+    '''
+    A Hangman Game that asks the user for a letter and checks if it is in the word.
+    It starts with a default number of lives and a random word from the word_list.
+    
+    Parameters:
+    ----------
+    word_list: list
+        List of words to be used in the game
+    num_lives: int
+        Number of lives the player has
+    
+    Attributes:
+    ----------
+    word: str
+        The word to be guessed picked randomly from the word_list
+    word_guessed: list
+        A list of the letters of the word, with '_' for each letter not yet guessed
+        For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']
+        If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
+    num_letters: int
+        The number of UNIQUE letters in the word that have not been guessed yet
+    num_lives: int
+        The number of lives the player has
+    list_of_guesses: list
+        A list of the letters that have already been tried
+    
+    Methods:
+    -------
+    check_guess(guess)
+        Checks if the letter is in the word.
+    ask_for_input()
+        Asks the user for a letter.
+    '''
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list) # The word to be guessed, picked randomly from the word_list.
         self.word_guessed = ['_'] * len(self.word)# A list of '_'s, the number of which is the len(self.word)
         self.num_letters = len(list(set(self.word))) # Length of the list of the unique letters in word.
-        '''#The number of UNIQUE letters in the word that have not been guessed yet.
-        #Two conditions
-        #1: the letters have to have not been guessed yet, so still '_'s.
-        #2: the letters must be unique.
-        
-        #First idea:
-        #testing the limits of isalpha.
-            #txt = '_'
-            #x = txt.isalpha()
-            #print(x) # False
-        #testing the limits of isalpha.
-        #learned that _ is going to be not considered the same as a, or b, or c etc...
-        #so can distinguish between alphabeticals and _s.
-        #question: can I count the number of '_'s in a list?
-        #it will have to involve checking if each element of a list isalpha'''
         self.num_lives = num_lives # Number of lives at the start.
         self.word_list = word_list # The list of words.
         self.list_of_guesses = [] # A list of the guesses that have already been tried.
     
     def check_guess(self, guess):
+        '''
+        Checks if the letter is in the word.
+        If it is, it replaces the '_' in the word_guessed list with the letter.
+        If it is not, it reduces the number of lives by 1.
+        
+        Parameters:
+        ----------
+        guess: str
+            The letter to be checked
+        '''
         guess = guess.lower() # Ensures guess is lowercase.
         indices = [i for i, x in enumerate(self.word) if x == guess] # list of indices where guess in word.
         lst_of_replacement_strings = [guess] * self.word.count(guess) # list of strings, all are guess.
@@ -49,6 +77,12 @@ class Hangman:
             print(f"You have {self.num_lives} lives left.")
         
     def ask_for_input(self):
+        '''
+        Asks the user for a letter and checks two things:
+        1. If the letter has already been tried
+        2. If the character is a single character
+        If it passes both checks, it calls the check_letter method.
+        '''
         while True:
             guess = input("Please enter a single letter: ")
             if len(guess) != 1 or guess.isalpha() != True:
